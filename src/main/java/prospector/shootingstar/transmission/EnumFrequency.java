1d4520979aa2b7c87ce7fd4afe3b23ee90e965dc
Prospector <thedoctorsoda@gmail.com>
2017-05-16 18:25:51 -0700

    Add Transmission

---
+++ src/main/java/prospector/shootingstar/transmission/EnumFrequency.java
@@ -0,0 +1,5 @@
+package prospector.shootingstar.transmission;
+
+public enum EnumFrequency {
+	ALPHA, BETA, GAMMA, DELTA, EPSILON, ZETA, ETA, THETA, IOTA, KAPPA
+}
---
+++ src/main/java/prospector/shootingstar/transmission/ITransmissionReciever.java
@@ -0,0 +1,40 @@
+package prospector.shootingstar.transmission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ITransmissionReciever {
+
+    public default boolean isPowered(EnumFrequency frequency) {
+        if (getValidEnumFrequencies().contains(frequency))
+            for (Frequency f : getValidFrequencies()) {
+                if (f.enumFrequency.equals(frequency) && f.isPowered()) {
+                    return true;
+                }
+            }
+        return false;
+    }
+
+    public List<Frequency> getValidFrequencies();
+
+    public default List<EnumFrequency> getValidEnumFrequencies() {
+        List<EnumFrequency> enumFrequencies = new ArrayList<>();
+        for (Frequency frequency : getValidFrequencies())
+            enumFrequencies.add(frequency.enumFrequency);
+        return enumFrequencies;
+    }
+
+    @SuppressWarnings("deprecation")
+    public default void setPowered(EnumFrequency frequency, boolean power) {
+        if (getValidEnumFrequencies().contains(frequency)) {
+            for (Frequency f : getValidFrequencies()) {
+                if (f.enumFrequency.equals(frequency)) {
+                    f.setPowered(power);
+                    onPowerUpdate();
+                }
+            }
+        }
+    }
+
+    public void onPowerUpdate();
+}
