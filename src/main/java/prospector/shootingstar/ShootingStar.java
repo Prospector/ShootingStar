b80ad08a3258d5481f7e8bd344d99bd18dd4dd31
Prospector <thedoctorsoda@gmail.com>
2017-07-06 14:21:31 -0700

    Item/Block registry

--- src/main/java/prospector/shootingstar/ShootingStar.java
+++ src/main/java/prospector/shootingstar/ShootingStar.java
@@ -1,8 +1,14 @@
 package prospector.shootingstar;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
 import prospector.shootingstar.model.ModelCompound;
 import prospector.shootingstar.model.ModelMethods;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +16,28 @@ import static prospector.shootingstar.model.ModelMethods.registerItemModel;
 
 public class ShootingStar {
     protected static List<ModelCompound> modelList = new ArrayList<>();
+    protected static List<BlockCompound> blockList = new ArrayList<>();
+    protected static List<ItemCompound> itemList = new ArrayList<>();
+
+    public static Block getBlock(String modid, String name) {
+        for (BlockCompound compound : blockList) {
+            if (compound.getRegistryName().equals(new ResourceLocation(modid, name))) {
+                return compound.getBlock();
+            }
+        }
+        throw new InvalidParameterException("Block '" + modid + ":" + name + "' cannot be found in the Shooting Star registry");
+    }
 
     public static void registerModel(ModelCompound modelCompound) {
         modelList.add(modelCompound);
+    }
+
+    public static void registerBlock(BlockCompound blockCompound) {
+        blockList.add(blockCompound);
+    }
+
+    public static void registerItem(ItemCompound itemCompound) {
+        itemList.add(itemCompound);
     }
 
     public static void registerModels(String modid) {
@@ -30,6 +55,31 @@ public class ShootingStar {
                     else
                         ModelMethods.setBlockStateMapper(compound.getBlock(), compound.getFileName(), compound.getBlockStatePath(), compound.getIgnoreProperties());
                 }
+            }
+        }
+    }
+
+    public static void registerBlocks(String modid, RegistryEvent.Register<Block> event) {
+        for (BlockCompound compound : blockList) {
+            if (compound.getModid().equals(modid)) {
+                event.getRegistry().register(compound.getBlock());
+            }
+        }
+    }
+
+    public static void registerItems(String modid, RegistryEvent.Register<Item> event) {
+        for (BlockCompound compound : blockList) {
+            if (compound.hasItemBlock() && compound.getModid().equals(modid)) {
+                if (compound.getItem() == null) {
+                    event.getRegistry().register(new ItemBlock(compound.getBlock()).setRegistryName(compound.getBlock().getRegistryName()));
+                } else {
+                    event.getRegistry().register(compound.getItem());
+                }
+            }
+        }
+        for (ItemCompound compound : itemList) {
+            if (compound.getModid().equals(modid)) {
+                event.getRegistry().register(compound.getItem());
             }
         }
     }
